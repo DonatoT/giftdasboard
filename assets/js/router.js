d91911dab85d0fb77fbc96bac7b45ce70cb72b26
Alaindongmo <guillaumeden61@gmail.com>
2023-01-04 16:52:55 +0100

    v3

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,5 +1,5 @@
 
-  function openpage(evt, page) {
+  function openPage(evt, Page) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -9,7 +9,7 @@
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(page).style.display = "block";
+    document.getElementById(Page).style.display = "block";
     evt.currentTarget.className += "active";
   }
   document.getElementById("defaultOpen").click();
---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function actif(id) {
+    document.getElementById('1').style.color='#ffffff';
+    document.getElementById('2').style.color='#ffffff';
+    document.getElementById('3').style.color='#ffffff';
+    document.getElementById('4').style.color='#ffffff';
+    document.getElementById(id).style.color='#FF9D41';
+}
